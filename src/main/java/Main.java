239d9ab38d812f0345eb06738543804c3a65f525
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 22:49:02 +0300

    Scripted auto-commit on change (2021-12-04 22:49:02) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ import akka.stream.ActorMaterializer;
 public class Main {
     public static void main(String[] args) {
         ActorSystem system = ActorSystem.create("simplest-test");
-        ActorMaterializer materializer = ActorMaterializer.create(system);
+        final ActorMaterializer materializer = ActorMaterializer.create(system);
         final Http http = Http.get(system);
-        
+
 }
