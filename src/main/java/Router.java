066ca095984eab6c8d8eaa292f5f4fc397527236
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-05 00:06:12 +0300

    Scripted auto-commit on change (2021-12-05 00:06:12) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -13,6 +13,7 @@ import akka.stream.javadsl.Source;
 
 import java.time.Duration;
 import java.util.Collections;
+import java.util.Optional;
 import java.util.concurrent.CompletionStage;
 
 import static akka.http.javadsl.server.Directives.*;
@@ -31,9 +32,7 @@ public  class MainHttp {
                             flow.map(req -> new Pair(testUrl, count)).mapAsync(0, pair -> {
                                 CompletionStage<Object> result = Patterns.ask(cacheActor, new CachingActor.GetMessage(testUrl), TIMEOUT)
                                         .thenCompose(m -> {
-                                            if(m.equals(null)) {
-
-                                            }
+                                            Optional.ofNullable(m)
                                         });
                             });
                             Flow<HttpRequest, Pair<String, Integer>, NotUsed> mapped = flow.map(req -> new Pair(testUrl, count));
