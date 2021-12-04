0ff76496c3f354e2eae5ba440fa0341fff70f7c0
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 23:02:00 +0300

    Scripted auto-commit on change (2021-12-04 23:02:00) by gitwatch.sh

--- src/main/java/CachingActor.java
+++ src/main/java/CachingActor.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class CachingActor {
+import akka.actor.AbstractActor;
+import akka.japi.pf.ReceiveBuilder;
+
+public class CachingActor extends AbstractActor {
+    @Override
+    public Receive createReceive() {
+        return ReceiveBuilder.create().match(StoreMessage).build();
+    }
 }
