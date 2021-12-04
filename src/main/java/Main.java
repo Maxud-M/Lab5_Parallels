5f309063280e0c62be531654742d3da7ce44acdc
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 22:55:33 +0300

    Scripted auto-commit on change (2021-12-04 22:55:33) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,5 +16,5 @@ public class Main {
         final Http http = Http.get(system);
         final Flow<HttpRequest, HttpResponse, NotUsed> routeFlow = MainHttp.createRoute(materializer, system, http)
                 .flow(system, materializer);
-        final CompletionStage<ServerBinding> 
+        final CompletionStage<ServerBinding> binding 
 }
