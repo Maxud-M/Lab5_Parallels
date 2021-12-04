89f972514ec84d1d30215484301801aeb6d42ca0
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 22:56:47 +0300

    Scripted auto-commit on change (2021-12-04 22:56:47) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -22,5 +22,5 @@ public class Main {
                 ConnectHttp.toHost("localhost", 8080),
                 materializer
         );
-        System.out.println("Server online at ");
+        System.out.println("Server online at http://");
 }
