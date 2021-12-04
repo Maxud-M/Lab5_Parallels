33e55404b125d376dd9b70d6ac33ba0cec169a5c
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 22:47:27 +0300

    Scripted auto-commit on change (2021-12-04 22:47:27) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,9 @@
+import akka.actor.ActorSystem;
+import akka.stream.ActorMaterializer;
+
 public class Main {
     public static void main(String[] args) {
-        ActorSystem system = ActorSystem.create(responsive) "simplest-ом в ответ на запрос события test");
-        ActorMaterializer materializer = ActorMaterializer.create(responsive) system);
+        ActorSystem system = ActorSystem.create("simplest-ом в ответ на запрос события test");
+        ActorMaterializer materializer = ActorMaterializer.create(system);
     }
 }
