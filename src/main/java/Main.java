8d9a79ddd4b35376d3991ac150140dd2c0cb9039
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 22:46:42 +0300

    Scripted auto-commit on change (2021-12-04 22:46:42) by gitwatch.sh

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        
+        ActorSystem system = ActorSystem.create(responsive) "simplest-ом в ответ на запрос события test");
+        ActorMaterializer materializer = ActorMaterializer.create(responsive) system);
     }
 }
