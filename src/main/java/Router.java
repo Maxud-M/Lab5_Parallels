faa7667c9c87e0ad68f3d6a4f9cd2c71e54bdb21
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 23:43:54 +0300

    Scripted auto-commit on change (2021-12-04 23:43:54) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -22,7 +22,9 @@ public  class MainHttp {
                         parameter(QUERY_PARAMETR_COUNT, count -> {
                             Flow<HttpRequest, HttpRequest, NotUsed> flow = Flow.of(HttpRequest.class);
                             Flow<HttpRequest, Pair<String, Integer>, NotUsed> mapped = flow.map(req -> new Pair(testUrl, count));
-                            mapped.mapAsync(0, )
+                            mapped.mapAsync(0, pair -> {
+                                pair.
+                            })
 
 
                         }))));
