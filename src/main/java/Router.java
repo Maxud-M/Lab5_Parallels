14860dfdf8f87e15caff6af1b58fa8d2f1278e0a
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-04 23:27:51 +0300

    Scripted auto-commit on change (2021-12-04 23:27:50) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -2,8 +2,10 @@ import akka.NotUsed;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
+import akka.http.javadsl.model.HttpResponse;
 import akka.http.javadsl.server.Route;
 import akka.stream.ActorMaterializer;
+import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Source;
 
 import static akka.http.javadsl.server.Directives.*;
@@ -17,7 +19,7 @@ public  class MainHttp {
         return route(get(
                 () -> parameter(QUERY_PARAMETR_URL, testUrl ->
                         parameter(QUERY_PARAMETR_COUNT, count -> {
-                            Source<HttpRequest, NotUsed> source = Flow.of()
+                            Flow<HttpRequest, HttpResponse, NotUsed> source = Flow.of(HttpRequest.class);
                         }))));
 
     }
