8efe6b47885bfa784fed17c4fd8ed3e0ae71f619
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-05 00:01:50 +0300

    Scripted auto-commit on change (2021-12-05 00:01:49) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -1,4 +1,5 @@
 import akka.NotUsed;
+import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.http.javadsl.Http;
 import akka.http.javadsl.model.HttpRequest;
@@ -10,6 +11,7 @@ import akka.stream.ActorMaterializer;
 import akka.stream.javadsl.Flow;
 import akka.stream.javadsl.Source;
 
+import java.time.Duration;
 import java.util.Collections;
 import java.util.concurrent.CompletionStage;
 
@@ -17,21 +19,21 @@ import static akka.http.javadsl.server.Directives.*;
 
 public  class MainHttp {
 
+    static final Duration TIMEOUT = Duration.ofSeconds(5);
     public static final String QUERY_PARAMETR_URL = "testUrl";
     public static final String QUERY_PARAMETR_COUNT = "count";
 
-    public static Route createRoute(ActorMaterializer materializer, ActorSystem system, Http http) {
+    public static Route createRoute(ActorMaterializer materializer, ActorSystem system, Http http, ActorRef cacheActor) {
         return route(get(
                 () -> parameter(QUERY_PARAMETR_URL, testUrl ->
                         parameter(QUERY_PARAMETR_COUNT, count -> {
                             Source.from(Collections.singletonList())
                             Flow<HttpRequest, HttpRequest, NotUsed> flow = Flow.of(HttpRequest.class);
                             flow.map(req -> new Pair(testUrl, count)).mapAsync(0, pair -> {
-                                Patterns.ask()
+                                CompletionStage<Object> result = Patterns.ask(cacheActor, new CachingActor.GetMessage(testUrl), TIMEOUT);
                             });
                             Flow<HttpRequest, Pair<String, Integer>, NotUsed> mapped = flow.map(req -> new Pair(testUrl, count));
                             Flow<Pair<String, Integer>, Pair<String, Integer>, NotUsed> f = Flow.<Pair<String, Integer>> create();
-                            f.mapConcat()
 
 
                         }))));
