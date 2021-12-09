efcc80ad494c5794b50b9016ea00fc89f4668166
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-09 21:14:36 +0300

    Scripted auto-commit on change (2021-12-09 21:14:36) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -68,7 +68,7 @@ public  class MainHttp {
                                                     return whenResponse;
                                                 });
                                                 //Sink<Long, CompletionStage<Long>> fold = flowMapped.fold(0, (agg, next) -> agg + next);
-                                                Sink<Pair<String, Integer>, CompletionStage<Long>> testSink = flowMapped.fold(0, (agg, next) -> agg + next).toMat(Sink.head(), Keep.right());
+                                                Sink<Pair<String, Integer>, CompletionStage<Long>> testSink = flowMapped.fold(Long(0), (agg, next) -> agg + next).toMat(Sink.head(), Keep.right());
                                                 RunnableGraph<CompletionStage<Long>> graph = Source.from(Collections.singletonList(new Pair(testUrl, count))).toMat(testSink, Keep.right());
                                                 CompletionStage<Long> result = graph.run(materializer);
                                                 return result;
