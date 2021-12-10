d2950715e8daf9b65b69504dc11a42270b1ebb48
Maxud-M <maksud.maksudov.00@mail.ru>
2021-12-10 03:39:26 +0300

    Scripted auto-commit on change (2021-12-10 03:39:25) by gitwatch.sh

--- src/main/java/Router.java
+++ src/main/java/Router.java
@@ -42,8 +42,8 @@ public  class Router {
                         parameter(QUERY_PARAMETR_COUNT, count -> {
                             int numOfReq = Integer.parseInt(count);
                             HttpRequest request1 = HttpRequest.create("http://localhost:8080/?testUrl=" + testUrl + "&count=" + count);
-                            System.out.println(request.getUri().query().toList());
-                            System.out.println(request.entity());
+                            System.out.println(request1.getUri().query().toList());
+                            System.out.println(request1.entity());
                             Flow<HttpRequest, HttpRequest, NotUsed> flow = Flow.of(HttpRequest.class);
                             Flow<HttpRequest, Pair<String, Integer>, NotUsed> mapped = flow.map(req -> new Pair<>(testUrl, numOfReq));
                             Flow<HttpRequest, Long, NotUsed> m = mapped.mapAsync(1, p ->
